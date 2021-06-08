79354da74cd259167bdb66aba629b2946247edc5
user42 <viniciuscordeirop@gmail.com>
2021-06-08 03:53:59 +0200

    Add get next line to header

--- include/libft.h
+++ include/libft.h
@@ -65,5 +65,6 @@ t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
 t_list				*ft_lstnew(void *content);
 int					ft_lstsize(t_list *lst);
+int					get_next_line(int fd, char **line);
 
 #endif
