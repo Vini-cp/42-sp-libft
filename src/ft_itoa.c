5addcd9d2e4fe0196edac24f830e42069620fb22
ViniciusCordeiroP <viniciuscordeirop@gmail.com>
2021-06-08 14:23:32 -0300

    Fix ft_itoa

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -1,10 +1,18 @@
 #include "../include/libft.h"
 
+static	void	negative_nbr(int *num, int *negative)
+{
+	*num *= -1;
+	*negative = 1;
+}
+
 char	*ft_itoa(int num)
 {
 	int		i;
+	int		negative;
 	char	*str;
 
+	negative = 0;
 	if (num == 0)
 		return (str = ft_strdup("0\0"));
 	else if (num == -2147483648)
@@ -12,13 +20,13 @@ char	*ft_itoa(int num)
 	i = 0;
 	str = (char *)malloc(12 * sizeof(char));
 	if (ft_isnegative(num))
-		num *= -1;
+		negative_nbr(&num, &negative);
 	while (num != 0)
 	{
 		str[i++] = num % 10 + '0';
 		num = num / 10;
 	}
-	if (ft_isnegative(num))
+	if (negative)
 		str[i++] = '-';
 	str[i] = '\0';
 	ft_strrev(str);
